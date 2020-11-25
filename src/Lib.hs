324de0b16e3b316255f42a957b205231fa2e126d
sjqtenticles <allen.sarkisyan@warnerbros.com>
2020-11-24 21:05:24 -0800

    making progress, adding some more types, getting some basic search paths going

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,10 +1,42 @@
+{-# LANGUAGE RecordWildCards #-}
+
 module Lib
     ( someFunc
     ) where
 
+import Data.List
+
+data Entity = Entity {
+    symbol :: Char,
+    name :: String,
+    loc :: Loc,
+    control :: Space
+} deriving (Show, Eq)
+
 data Loc = Point Int Int deriving (Show, Eq)
+
 type Space = [Loc]
+
 type Depth = Int
+
+data World = World {
+    steps :: Int,
+    entities :: [Entity]
+} deriving (Show, Eq)
+
+player :: Entity
+player = Entity {
+    symbol = 'P', 
+    name = "Player", 
+    loc = Point 0 0 , 
+    control = adjacentPoints (Point 0 0)
+    }
+
+initializeWorld :: World
+initializeWorld = World { steps = 0, entities = []}
+
+spawn :: World -> Entity -> World
+spawn World{..} e = World { entities = e : entities, .. }
 
 adjacentPoints :: Loc -> Space
 adjacentPoints (Point x y) = [
@@ -17,11 +49,15 @@ adjacentPoints (Point x y) = [
     (Point x (y-1)),
     (Point (x-1) (y-1))]
 
-nearbyPoints :: Loc -> Depth -> Space -- need to optimize this shit with memoization
-nearbyPoints loc 0 = []
-nearbyPoints loc depth = loc : (concat $ map (\x -> nearbyPoints x (depth-1)) (adjacentPoints loc))
+nearbyPoints :: Loc -> Depth -> Space
+nearbyPoints p 0 = []
+nearbyPoints p 1 = adjacentPoints p
+nearbyPoints p n = union (adjacentPoints p) (concatMap adjacentPoints (nearbyPoints p (n-1)))
+
+printPoints :: Space -> IO ()
+printPoints s = mapM_ (putStrLn . show) s
 
 someFunc :: IO ()
 someFunc = do
-    let moves = nearbyPoints (Point 0 0) 3
-    mapM_ (putStrLn . show) moves
+    let p = nearbyPoints (Point 0 0) 5
+    printPoints p
